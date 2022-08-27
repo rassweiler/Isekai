db1cb0b94b7d63a85a196bfc65a8da95a8107ea4
Kyle Rassweiler <dev@kylerassweiler.ca>
2022-08-27 08:05:13 -0400

    Add mass to motion physics

--- Characters/first_person_camera_rigidbody/first_person_camera_rigid.gd
+++ Characters/first_person_camera_rigidbody/first_person_camera_rigid.gd
@@ -1,13 +1,12 @@
 extends RigidDynamicBody3D
 
-@export var acceleration = 15.0
-@export var deceleration = 1.0
-@export var jump_velocity = 5.0
-@export var speed = 15.0
-@export var max_speed = 15.0
+@export var acceleration = 1.2
+@export var jump_velocity = 150.0
+@export var speed = 900.0
+@export var max_speed = 900.0
 @export var view_sensitivity = 10.0
 @export var max_pick_weight = 90.0
-@export_range(0.01,1.0) var stop_speed = 0.1
+@export_range(0.001,1.0) var stop_speed = 0.01
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -43,8 +42,8 @@ func _integrate_forces(state):
 		state.linear_velocity = state.linear_velocity.normalized() * max_speed
 		
 	if move_input.length() < 0.2:
-		state.linear_velocity.x = lerp(state.linear_velocity.x, 0.0, stop_speed)
-		state.linear_velocity.z = lerp(state.linear_velocity.z, 0.0, stop_speed)
+		state.linear_velocity.x = lerp(state.linear_velocity.x, 0.0, stop_speed * mass)
+		state.linear_velocity.z = lerp(state.linear_velocity.z, 0.0, stop_speed * mass)
 		
 	if state.get_contact_count() > 0 and move_input.length() < 0.2:
 		if is_on_floor and state.get_contact_local_normal(0).y < 0.9:
@@ -57,16 +56,15 @@ func _physics_process(delta):
 	var direction = Vector3()
 	move_input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	direction = (neck.transform.basis * Vector3(move_input.x, 0, move_input.y)).normalized()
-	velocity = lerp(velocity, direction * speed, acceleration * deceleration * delta)
+	velocity = lerp(velocity, direction * speed, acceleration * mass * delta)
 	apply_central_force(velocity)
 	
 	if feet.is_colliding():
 		is_on_floor = true
-		deceleration = 0.5
 		physics_material_override.friction = 0.1
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor:
-		deceleration = 0.1
 		is_on_floor = false
-		apply_central_impulse(Vector3.UP * jump_velocity)
+		print("jumping")
+		apply_central_impulse(Vector3.UP * jump_velocity * mass)
 
